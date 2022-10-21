54d56aadec70deddabd3fd7931575c5e69b96f8f
mugus <muhozagustave1213@gmail.com>
2022-10-21 12:00:25 +0200

    survey as customer end

--- screens/components/SurveyForm.js
+++ screens/components/SurveyForm.js
@@ -9,11 +9,12 @@ import {
   } from '@expo-google-fonts/roboto';
 import RadioGroup from 'react-native-radio-buttons-group';
 import color from '../layouts/color';
-// import Slider from '@react-native-community/slider';
-
 const KEY = '@@KEY';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+// import Slider from '@react-native-community/slider';
+
+
 
 
 const radioButtonsData = [
@@ -103,7 +104,7 @@ if (!fontsLoaded) {
                         <View style={{flex: 1, flexDirection: 'row',padding: 5}}>
                             <View style={{flexDirection: 'column'}}>
                                 <Text style={{fontSize: 14, fontWeight: 'bold', textTransform: 'uppercase', alignSelf: 'center', paddingBottom: 20 ,color: color.APP_PRIMARY}}>
-                                    Uzuza nk'umushyitsi
+                                    Umushyitsi
                                 </Text>
                                 <View style={{flexDirection: 'row'}}>
                                     <View style={styles.namesContainer}>
@@ -187,12 +188,127 @@ if (!fontsLoaded) {
                     lang === 1 ?
                     <>
                         <View style={{flex: 1, flexDirection: 'row',padding: 5}}>
-                            <Text>Wemeje ko uri Umukiriya</Text>
+                            <View style={{flexDirection: 'column'}}>
+                                <Text style={{fontSize: 14, fontWeight: 'bold', textTransform: 'uppercase', alignSelf: 'center', paddingBottom: 20 ,color: color.APP_PRIMARY}}>
+                                    Umukiriya
+                                </Text>
+                                <View style={{flexDirection: 'row', width: width-30,paddingLeft: 10}}>
+                                    <View style={styles.namesContainer}>
+                                        <TextInput placeholder="Izina" style={styles.names}/>
+                                    </View>
+                                    <Text>  </Text>
+                                    <View style={styles.namesContainer}>
+                                        <TextInput placeholder="Andi mazina" style={styles.names} />
+                                    </View>
+                                </View>
+                                
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni gihe ki ukoresha imiti yacu?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni gute wagereranya ibicuruzwa byacu nabandi bacuruza nkibyacu?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni ibihe bintu by'ingenzi tubura(Twakongera kuri serivisi dutanga)?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni iki ugerageza gukemura ukoresheje imiti yacu?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni abahe bantu bandi imiti yacu ubona ishobora kugirira akamaro?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Biroroshye gukoresha imiti yacu?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ubona imiti yacu wayirangira abandi batayikoresha?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="Ni gute dushobora kunoza imikorere kugirango duhuze neza ibyo ukeneye?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.submitButton}>
+                                    <Button title='Submit'
+                                            buttonStyle={{
+                                                backgroundColor: color.APP_PRIMARY,
+                                                borderWidth: 1,
+                                                color: '#fff',
+                                                borderColor: color.APP_PRIMARY,
+                                                borderRadius: 5,
+                                            }}/>
+                                </View>
+                            </View>
                         </View>
+
                     </>:
                     <>
                         <View style={{flex: 1, flexDirection: 'row',padding: 5}}>
-                            <Text>Survey Form as Customer</Text>
+
+                            <View style={{flexDirection: 'column'}}>
+                                <Text style={{fontSize: 14, fontWeight: 'bold', textTransform: 'uppercase', alignSelf: 'center', paddingBottom: 20 ,color: color.APP_PRIMARY}}>
+                                    Customer
+                                </Text>
+                                <View style={{flexDirection: 'row', width: width-30,paddingLeft: 10}}>
+                                    <View style={styles.namesContainer}>
+                                        <TextInput placeholder="Firstname" style={styles.names}/>
+                                    </View>
+                                    <Text>  </Text>
+                                    <View style={styles.namesContainer}>
+                                        <TextInput placeholder="Lastname" style={styles.names} />
+                                    </View>
+                                </View>
+                                
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="How often do you use our products?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="How would you compare our products to our competitors?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="What important features are we missing?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="What are you trying to solve by using our product?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="What other types of people could find our product useful?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="How easy is it to use our product?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="How likely are you to recommend this product to others?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.messageContainer}>
+                                    <TextInput multiline placeholder="How could we improve our product to better meet your needs?" numberOfLines={4} style={styles.message} />
+                                </View>
+
+                                <View style={styles.submitButton}>
+                                    <Button title='Submit'
+                                            buttonStyle={{
+                                                backgroundColor: color.APP_PRIMARY,
+                                                borderWidth: 1,
+                                                color: '#fff',
+                                                borderColor: color.APP_PRIMARY,
+                                                borderRadius: 5,
+                                            }}/>
+                                </View>
+                            </View>
+
                         </View>
                     </>
                 }
@@ -212,9 +328,10 @@ const styles =  StyleSheet.create({
 
     },
     names: {
-        width: (width/2)-10,
-        borderWidth: 0.5,
-        borderRadius: 5,
+        width: (width/2)-20,
+        // borderWidth: 0.5,
+        // borderRadius: 5,
+        borderBottomWidth: 1,
         borderColor: color.APP_PRIMARY,
         paddingLeft: 10,
         padding: 2,
@@ -225,6 +342,10 @@ const styles =  StyleSheet.create({
     msgContainer : {
         paddingTop: 6,
         color: color.APP_PRIMARY
+    },
+    messageContainer:{
+        width: width-30,
+        paddingLeft: 10
     },
     message: {
         paddingTop: 10,
@@ -244,9 +365,8 @@ const styles =  StyleSheet.create({
     },
     submitButton :{
         paddingTop: 20,
-        width: width- 50,
-        alignContent: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center'
+        width: width-30,
+        paddingLeft: 10,
+        color: color.APP_PRIMARY
     }
 })
